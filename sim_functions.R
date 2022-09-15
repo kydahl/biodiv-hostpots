2cdebc1ad9ee31c0a88d0eb5bc4156c70e4515b1
Kyle Dahlin <kydahlin@gmail.com>
2022-09-15 17:21:58 -0400

    !!! broken but will return to it

--- sim_functions.R
+++ sim_functions.R
@@ -10,72 +10,91 @@ library(tidyverse)
 ## Vulnerability function-------------------------------------------------------
 # take traits, current state, and patch number and outputs future state
 
-# simple function for now: 
+# simple function for now:
 # if average of traits > 0.5, increase IUCN status
 # if average of traits < -0.5, decrease IUCN status
 # otherwise maintain status
-vulner_func <- function(in_df, in_year) {
+get.vulnerability <- function(in_df, in_year) {
   out_df <- in_df %>%
-    filter(year == in_year) %>% 
-    rowwise() %>% 
+    # select the correct year
+    filter(year == in_year) %>%
+    # group by all columns except traits
+    group_by(across(c(-trait.num, -trait.val)), .drop = FALSE) %>%
     # compute average of traits
-    # TODO replace to use starts_with("trait") somehow
-    mutate(trait_avg = mean(c(trait1, trait2, trait3, trait4))) %>%
-    mutate(trait1 = rnorm(1)) %>% # one trait changes over time due to environmental changes, say
-    # assign new states
+    mutate(trait_avg = mean(trait.val)) %>%
+    # assign new states based on average of traits (in future, this will be a real function based on biology)
     mutate(state = state + case_when(
-      state == 1       ~ ifelse(rnorm(1) > 3, 1, 0), #2.326, 1, 0), # with small prob., extirpated species re-emerge
-      trait_avg > 0.5  ~ 1,
+      state == 1 ~ ifelse(rnorm(1) > 3, 1, 0), # 2.326, 1, 0), # with small prob., extirpated species re-emerge
+      trait_avg > 0.5 ~ 1,
       trait_avg < -0.5 ~ -1,
-      TRUE             ~ 0)
-      ) %>% 
-    mutate(state = ifelse(state > 6, 6, state)) %>% # 6 is the highest IUCN level (Least concern)
-    mutate(year = year + 1) %>% 
-    select(-trait_avg) %>% 
+      TRUE ~ 0
+    )) %>%
+    # 6 is the highest IUCN level (Least concern), so don't go higher than 6
+    mutate(state = ifelse(state > 6, 6, state)) %>%
+    mutate(year = year + 1) %>%
+    mutate(trait.val = case_when(
+      trait.num == 1 ~ rnorm(1), # one trait changes randomly over time due to environmental changes, say
+      TRUE ~ trait.val
+    )) %>%
+    select(-trait_avg) %>%
     ungroup()
 }
 
 ## Biodiversity metric functions------------------------------------------------
-# for a patch, look at entities and their states & traits and output 
+# for a patch, look at entities and their states & traits and output
 # biodiversity measure of the patch
 
 # simple function for now:
 # number of unique entityIDs in a patch
-biodiv_1 <- function(in_df) {
+calc.biodiv_1 <- function(in_df) {
   out_df <- in_df %>%
     filter(state > 1) %>% # remove extinct entities
-    select(patch, year, entityID) %>% 
-    count(patch, year)
+    select(patch, year, entityID) %>%
+    count(patch, year, name = "biodiv1")
 }
 
 # Number of endemic entities
-biodiv_2 <- function(in_df) {
-  out_df <- in_df %>% 
+calc.biodiv_2 <- function(in_df) {
+  out_df <- in_df %>%
     filter(state > 1) %>% # remove extinct entities
-    select(patch, year, entityID) %>%  
+    select(patch, year, entityID) %>%
     # only keep rows where entityID and year are unique ( == endemics)
-    group_by(entityID, year) %>% 
-    filter(n() == 1) %>% 
-    ungroup() %>% 
+    group_by(entityID, year) %>%
+    # remove duplicates due to rows from trait.num
+    distinct() %>%
+    filter(n() == 1) %>%
+    ungroup() %>%
     # count number of endemics in each patch
-    count(patch, year)
+    count(patch, year, name = "biodiv2")
 }
 
 
 ## Hotspot identifier function--------------------------------------------------
-# look at the biodiversity of all patches and determine which patches are 
+# look at the biodiversity of all patches and determine which patches are
 # "hotspots" relative to the other patches
 
 # simple function for now:
-# just choose the top 5 patches in terms of biodiversity
+# just choose the top patch in terms of biodiversity
+find.hotspots <- function(in_df, biod_metric) {
+  out_df <- in_df %>%
+    select(patch, year, !!as.name(biod_metric)) %>%
+    group_by(year) %>%
+    slice_max(order_by = !!as.name(biod_metric), n = 1, with_ties = FALSE) %>%  # !!! this breaks ties arbitrarily
+    ungroup()
+}
 
 
 ## Hotspot comparison function--------------------------------------------------
-# measure the difference in the hotspot maps produced under different 
+# measure the difference in the hotspot maps produced under different
 # biodiversity metrics for the same dataset
 
 # simple function for now:
 # measure the overlap in which patches were considered hotspots
-
-
+calc.hotspot_compare <- function(df1, df2) {
+  vec1 <- select(df1, patch)
+  vec2 <- select(df2, patch)
+  # get the Euclidean distance between the patch lists (!!! temporary)
+  norm(vec1-vec2, "2")
+  
+}
 
--- sim_traits.R
+++ sim_traits.R
@@ -5,32 +5,23 @@
 # Load libraries----------------------------------------------------------------
 library(tidyverse)
 
-# Helper functions--------------------------------------------------------------
-
-# Sample from a discrete uniform distribution
-# Source: https://stats.stackexchange.com/questions/3930/are-there-default-functions-for-discrete-uniform-distributions-in-r
-dunif_sampleone <- function(n) sample(1:n, 1, replace = T)
-
-# Assign entries from df to groups of size 1 to n (chosen uniformly randomly)
-group_assign <- function(df, n) sample(df, size = dunif_sampleone(n), replace = FALSE)
-
 # Parameters--------------------------------------------------------------------
 
 ## Random seed (to keep things consistent while coding at first)
-set.seed(8797)
+#set.seed(8797)
 
 ## Number of entities
 numEntities <- 1000 # got to 10 million before code took a while to run
 
 ## Number of traits
 # TODO automate number of traits (low priority as we'll probably be taking traits from a database)
-# numTraits <- 
+numTraits <- 4
 
 ## Number of patches
-# numPatches <- # TODO: automate number of patches
+numPatches <- 10 # TODO: automate number of patches
 
 ## Maximum number of entities per patch
-maxEntitiesPatch <- numEntities/20 # very arbitrary choice for now.
+maxEntitiesPatch <- numEntities/2 # very arbitrary choice for now.
 
 ## IUCN categories
 IUCN_cats <- c("Extinct",
@@ -41,48 +32,58 @@ IUCN_cats <- c("Extinct",
                "Least concern")
 state_list <- tibble(nums = 1:6, cats = IUCN_cats)
 
+# Helper functions--------------------------------------------------------------
+
+# Sample from a discrete uniform distribution
+# Source: https://stats.stackexchange.com/questions/3930/are-there-default-functions-for-discrete-uniform-distributions-in-r
+dunif_sampleone <- function(n) sample(1:n, 1, replace = T)
+
+# Assign entries from df to groups of size 1 to n (chosen uniformly randomly)
+group_assign <- function(df, n) sample(df, size = dunif_sampleone(n), replace = FALSE)
+
 # Create tables-----------------------------------------------------------------
 
 ## Define all the entities to be assigned to patches
-entity_df <- tibble(
-  # Assign traits (for now this is random) will be automated in future update
-  trait1 = rnorm(numEntities),
-  trait2 = rnorm(numEntities),
-  trait3 = rnorm(numEntities),
-  trait4 = rnorm(numEntities)
-) %>%
-  # Assign entityID (for now this is row number)
-  mutate(entityID = row_number())
+# Assign entity IDs and trait values
+entity_df <- tibble(expand.grid(entityID = 1:numEntities, 
+                         trait.num = 1:numTraits), 
+                    trait.val = rnorm(numEntities * numTraits))
+
+# # Assign traits ( these are currently IID normal(0, 1) )
+# for (i in 1:numTraits) {
+#   varname <- paste("trait", i , sep=".")
+#   entity_df[[varname]] <- rnorm(numEntities)
+# }
+
 
 ## Assign entities to patches (5 for now with a max of 25 entities in each)
 # will be automated in future update
 # uses a discrete uniform distribution to assign entities to patches for now
 regional_df <- tibble(
   entityID = NA,
-  patch = NA) %>%
-  add_row(patch = 1, entityID = group_assign(entity_df$entityID, maxEntitiesPatch)) %>%
-  add_row(patch = 2, entityID = group_assign(entity_df$entityID, maxEntitiesPatch)) %>%
-  add_row(patch = 3, entityID = group_assign(entity_df$entityID, maxEntitiesPatch)) %>%
-  add_row(patch = 4, entityID = group_assign(entity_df$entityID, maxEntitiesPatch)) %>%
-  add_row(patch = 5, entityID = group_assign(entity_df$entityID, maxEntitiesPatch)) %>%
-  add_row(patch = 6, entityID = group_assign(entity_df$entityID, maxEntitiesPatch)) %>%
-  add_row(patch = 7, entityID = group_assign(entity_df$entityID, maxEntitiesPatch)) %>%
+  patch = NA) 
+
+for (i in 1:numPatches) {
+  regional_df <- add_row(regional_df, patch = i, entityID = group_assign(entity_df$entityID, maxEntitiesPatch))
+}
+
+regional_df <- regional_df %>%
   mutate(uniqueID = paste("E", entityID, "_P", patch, sep = "")) %>% 
   filter(!is.na(entityID))
-
-## Expand the global entity data frame to include patch assignments
-full_df <- inner_join(entity_df, regional_df) %>%
-  relocate(entityID, patch, uniqueID)
 
 ## Assign states to entities in patches
 # done randomly for now
 
-full_df <- full_df %>%
+regional_df <- regional_df %>%
   # Assign states (except for "Extinct")
   # these are numbers with higher = better
-  mutate(state = sample(2:6, dim(full_df)[1], replace = TRUE)) %>% 
+  mutate(state = sample(2:6, dim(regional_df)[1], replace = TRUE)) %>% 
   mutate(year = 0) # initial year
 
+
+## Expand the global entity data frame to include patch assignments
+full_df <- inner_join(entity_df, regional_df) %>%
+  relocate(entityID, patch, uniqueID)
 
 
 
